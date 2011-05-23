03a1f4cc0d175316a69edcc92907e6aa09ac76fd
Andreas Zwinkau <zwinkau@kit.edu>
2011-05-23 13:35:57 +0200

    Add install script

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+if [ ! -f bashrc ]
+then
+	echo "Something is wrong. Where is bashrc?"
+	exit 1
+fi
+
+if [[ `hostname` == i44* ]]
+then
+	EMAIL="zwinkau@kit.edu"
+else
+	EMAIL="beza1e1@web.de"
+fi
+
+ln -sf bashrc ~/.bashrc
+ln -sf vimrc ~/.bashrc
+
+cp -f gitconfig ~/.gitconfig
+git config --global user.email ${EMAIL}
