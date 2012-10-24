300f079b043729238b30bcb0695fa3e9bdcf3979
Andreas Zwinkau <zwinkau@kit.edu>
2012-10-24 11:05:16 +0200

    More fish aliases

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -10,6 +10,22 @@ function g -d "alias for git"
 	git $argv
 end
 
+function gup -d "alias for g up"
+	g up $argv
+end
+
+function gst -d "alias for g st"
+	g st $argv
+end
+
+function gap -d "alias for g add -p"
+	g add -p $argv
+end
+
+function gg -d "alias for gitg"
+	gitg $argv
+end
+
 function go -d "alias for gnome-open"
 	gnome-open $argv
 end
@@ -24,6 +40,10 @@ end
 
 function todo -d "txt notebook"
 	vim ~/Dropbox/TODO.txt
+end
+
+function analyse_history -d "analyze fish shell history for often used commands"
+	grep -v "^#" $HOME/.config/fish/fish_history | sort | uniq -c | sort -n | tail
 end
 
 function LANGC -d "set LANG=C for a single command"
